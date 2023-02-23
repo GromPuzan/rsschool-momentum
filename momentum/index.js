a18b57d3c5edfd85612a49e40ac4125b0a9a4a95
Danil <GromPuzan@proton.me>
2023-02-23 15:39:32 +0300

    feat : add quotes

--- momentum/index.js
+++ momentum/index.js
@@ -15,11 +15,16 @@ const slideNext = document.querySelector(".slide-next");
 const slidePrev = document.querySelector(".slide-prev");
 const weatherIcon = document.querySelector(".weather-icon");
 const temperature = document.querySelector(".temperature");
-const wind = document.querySelector(".wind")
-const humidity = document.querySelector(".humidity")
+const wind = document.querySelector(".wind");
+const humidity = document.querySelector(".humidity");
 const weatherDescription = document.querySelector(".weather-description");
-const city = document.querySelector('.city');
-let cityInput = '';
+const city = document.querySelector(".city");
+
+const changeQuote = document.querySelector(".change-quote");
+const quote = document.querySelector(".quote");
+const author = document.querySelector(".author");
+let data = [];
+let cityInput = "";
 
 showTime();
 function showTime() {
@@ -68,22 +73,19 @@ function setBg() {
 }
 setBg();
 console.log(Number(randomNum));
-
 slidePrev.addEventListener("click", function clickPrev() {
-  if (randomNum == 1){
+  if (randomNum == 1) {
     randomNum = 20;
-  }
-  else {
+  } else {
     randomNum--;
   }
   setBg(randomNum, clickPrev);
 });
 
 slideNext.addEventListener("click", function clickNext() {
-  if (randomNum ==20) {
-    randomNum = 1
-  }
-  else {
+  if (randomNum == 20) {
+    randomNum = 1;
+  } else {
     randomNum++;
   }
   setBg(randomNum, clickNext);
@@ -93,20 +95,40 @@ async function getWeather() {
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&lang=ru&appid=2a5d749c64b251d9bd8104ad9c066de3&units=metric`;
   const res = await fetch(url);
   const data = await res.json();
-  weatherIcon.className = 'weather-icon owf';
+  weatherIcon.className = "weather-icon owf";
   weatherIcon.classList.add(`owf-${data.weather[0].id}`);
   temperature.textContent = `${data.main.temp}°C`;
   weatherDescription.textContent = data.weather[0].description;
   wind.textContent = `Скорость ветра: ${data.wind.speed}м/c`;
   humidity.textContent = `Влажность: ${data.main.humidity}%`;
   console.log(data.weather[0].id, data.weather[0].description, data.main.temp);
-
 }
 getWeather();
-city.addEventListener('change', () => {
+city.addEventListener("change", () => {
   cityInput = city.value;
   getWeather();
 });
+
+getQuotes();
+async function getQuotes() {
+  const quotes = "data.json";
+  const res = await fetch(quotes);
+  data = await res.json();
+  console.log(data);
+  showQuote();
+}
+// showQuote();
+
+function showQuote() {
+  const num = getRandomNum(1, data.length);
+  quote.textContent = data[num - 1].text;
+  author.textContent = data[num - 1].author;
+}
+
+changeQuote.addEventListener("click", () => {
+  showQuote();
+});
+
 
 
 function setLocalStorage() {
